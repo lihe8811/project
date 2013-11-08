f988e2f01f92f28637bbaa6f4bc44cd126ab8bb4
hli7 <hli@walmartlabs.com>
2013-11-07 21:53:09 -0800

    Learning Java

---
+++ java/welcome.java
@@ -0,0 +1,10 @@
+// Java program for learning
+// Author: He LI
+// Date: 2013-11-07
+// Version: 1.0
+
+public class Welcome{
+	public staiss void main(String[] args){
+		System.out.println("This is my first Java program!\n")
+	}
+}
