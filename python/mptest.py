52b52ad147dbaa952fa0a8b2f8862d313a83c5ff
hli7 <hli@walmartlabs.com>
2014-01-28 10:10:59 -0800

    test python multiprocessing

--- python/mptest.py
+++ python/mptest.py
@@ -9,3 +9,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import multiprocessing
+import time
+
+def worker(num):
+    """Worker calculate (input + 1).
+    """
+    pass
+    
+    time.sleep(20 - 2 * num)
+    print (num + 1)
+
+if __name__ == "__main__":
+    pool = multiprocessing.Pool(processes = 5)
+    result = []
+    for i in range(5):
+        result.append(pool.apply_async(worker, (i, )))
+    pool.close()
+    pool.join()
