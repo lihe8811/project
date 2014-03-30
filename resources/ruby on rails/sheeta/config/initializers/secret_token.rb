894c0375b45442f3c3e6a9f6fe36f902ee49ae62
hli7 <hli@walmartlabs.com>
2014-03-29 22:01:35 -0700

    start of project

---
+++ resources/ruby on rails/sheeta/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sheeta::Application.config.secret_token = 'a127c1b84369a449049b0ab7c4e30fbe4ed554bca5edb28a4c2d342dca49024ba6a9da9ecd01dd36a479b7d72d7d01e6d81f693746ebf472327d20a846340a1a'
