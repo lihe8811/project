8c8b8983cd760f1fc00f774f174be481728ac559
hli7 <hli@walmartlabs.com>
2014-04-03 21:00:21 -0700

    dynamic token & improve README

--- resources/ruby on rails/sheeta/config/initializers/secret_token.rb
+++ resources/ruby on rails/sheeta/config/initializers/secret_token.rb
@@ -4,4 +4,19 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sheeta::Application.config.secret_token = '67fad66cb0f645019a99b57824f30862c88b5b7d3ca13d584ada930897546c932ceef0518704bbfd168417c30cdc943dd3a81d13dc6b5907a7a7524bdb194c85'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use existing token
+    File.read(token_file).chomp
+  else
+    # Generate new token, store it in token_file
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Sheeta::Application.config.secret_key_base = secure_token
