744fc6c48ffed13cb5d98d431ae6ceda423a78b9
hli7 <hli@walmartlabs.com>
2014-05-17 07:24:10 -0700

    django study

---
+++ resources/django/sheeta/sheeta/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from sheeta.views import dateToday
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'sheeta.views.home', name='home'),
+    # url(r'^sheeta/', include('sheeta.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    
+    (r'^date/$', dateToday),
+    (r'^admin/$', include(admin.site.urls)),
+)
