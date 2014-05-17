696603ac3660a9dac31f2c038e36c9019100afab
hli7 <hli@walmartlabs.com>
2014-05-17 08:22:31 -0700

    admin

--- resources/django/sheeta/sheeta/urls.py
+++ resources/django/sheeta/sheeta/urls.py
@@ -14,8 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     
-    (r'^date/$', dateToday),
-    (r'^admin/$', include(admin.site.urls)),
+    url(r'^date/$', dateToday),
 )
